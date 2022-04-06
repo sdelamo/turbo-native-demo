df3b5010e7af4cff1fdbd4e11286a2df6d2acb69
Tim Yates <tim.yates@gmail.com>
2022-04-06 13:39:41 +0100

    Move existing demo, and create barebones micronaut app

---
+++ micronaut/src/test/groovy/io/micronaut/TurboSpec.groovy
@@ -0,0 +1,19 @@
+package io.micronaut
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import jakarta.inject.Inject
+
+@MicronautTest
+class TurboSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
