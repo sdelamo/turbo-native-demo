39a452c7a047f8783c284d8abfd0b44fb4cda66e
Tim Yates <tim.yates@gmail.com>
2022-04-07 12:37:44 +0100

    Add auth (first box in Advanced features)
    This adds a login form at '/signin', a logout form at '/signout'
    and a protected page.
    
    It also shows your name on the index page if you're signed in

--- Micronaut/build.gradle.kts
+++ Micronaut/build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
     implementation("io.micronaut:micronaut-validation")
+    implementation("io.micronaut.security:micronaut-security")
+    implementation("io.micronaut.security:micronaut-security-session")
+    implementation("io.projectreactor:reactor-core")
 
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
--- Micronaut/src/main/kotlin/io/micronaut/turbo/model/ViewModel.kt
+++ Micronaut/src/main/kotlin/io/micronaut/turbo/model/ViewModel.kt
@@ -1,11 +1,12 @@
 package io.micronaut.turbo.model
 
 import io.micronaut.core.annotation.Introspected
+import java.security.Principal
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 @Introspected
-data class ViewModel(val pageTitle: String, val pageClass: String = "", val action: String? = null) {
+data class ViewModel(val pageTitle: String, val pageClass: String = "", val action: String? = null, val principal: Principal? = null) {
     val renderDate: String get() = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.RFC_1123_DATE_TIME)
 }
