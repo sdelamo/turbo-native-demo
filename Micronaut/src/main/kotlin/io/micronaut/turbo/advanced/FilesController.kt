de7eaf0202979558d20ccd1c860e7f9808818a12
Tim Yates <tim.yates@gmail.com>
2022-04-07 13:34:39 +0100

    All the rest of the pages

---
+++ Micronaut/src/main/kotlin/io/micronaut/turbo/advanced/FilesController.kt
@@ -0,0 +1,16 @@
+package io.micronaut.turbo.advanced
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.turbo.model.ViewModel
+import io.micronaut.views.View
+
+@Controller("/files")
+class FilesController {
+
+    @View("files")
+    @Get(produces = [MediaType.TEXT_HTML], consumes = [MediaType.TEXT_HTML])
+    fun index() = ViewModel("Handling Files")
+
+}
---
+++ Micronaut/src/main/kotlin/io/micronaut/turbo/advanced/ModalController.kt
@@ -0,0 +1,21 @@
+package io.micronaut.turbo.advanced
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.turbo.model.ViewModel
+import io.micronaut.views.View
+import java.net.URI
+
+@Controller("/new")
+class ModalController {
+
+    @View("new")
+    @Get(produces = [MediaType.TEXT_HTML], consumes = [MediaType.TEXT_HTML])
+    fun index() = ViewModel("A Modal Webpage")
+
+    @Post(produces = [MediaType.TEXT_HTML], consumes = [MediaType.APPLICATION_FORM_URLENCODED, MediaType.TEXT_HTML])
+    fun post() = HttpResponse.temporaryRedirect<Any>(URI.create("/success"))
+}
