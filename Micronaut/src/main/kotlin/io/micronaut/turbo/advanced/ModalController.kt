c787e8988e66c5c72a28cdf1d04b29b07cdbc2fb
Tim Yates <tim.yates@gmail.com>
2022-04-07 13:49:34 +0100

    Fix the modal redirect

--- Micronaut/src/main/kotlin/io/micronaut/turbo/advanced/ModalController.kt
+++ Micronaut/src/main/kotlin/io/micronaut/turbo/advanced/ModalController.kt
@@ -1,12 +1,14 @@
 package io.micronaut.turbo.advanced
 
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.turbo.model.ViewModel
 import io.micronaut.views.View
+import io.netty.handler.codec.http.HttpHeaderNames
 import java.net.URI
 
 @Controller("/new")
@@ -17,5 +19,5 @@ class ModalController {
     fun index() = ViewModel("A Modal Webpage")
 
     @Post(produces = [MediaType.TEXT_HTML], consumes = [MediaType.APPLICATION_FORM_URLENCODED, MediaType.TEXT_HTML])
-    fun post() = HttpResponse.temporaryRedirect<Any>(URI.create("/success"))
+    fun post() = HttpResponse.status<Any>(HttpStatus.FOUND).header(HttpHeaderNames.LOCATION, "/success")
 }
